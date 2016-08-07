892f1d5dc5e4d52af01da2ed5fdfefc2484ff926
crash <oabtm@gmx.fr>
2016-08-07 16:19:43 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # getVideo
-Get the url of the first video on a given page.
+This package permits you to get the url of the first video on a given page,
+getVideo will use a headless browser if necessary.
+Here is an example:
+```
+var getVideo = require("getvideo");
+getVideo("https://youtube.com/watch?v=dQw4w9WgXcQ", (err, video) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(video)
+    }
+});
+```
---
+++ getVideo.js
@@ -0,0 +1,102 @@
+const parsonic = require("parsonic");
+const Nightmare = require("nightmare");
+const request = require("request");
+const url = require("url");
+const fs = require("fs");
+/*Most sites using Flash or Mediastream will send a regular video file
+if we're sending a mobile user-agent*/
+var userAgent = "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19";
+function getVideo(){
+    var videos = document.querySelectorAll("video, video > source");
+    for(var v = 0; v < videos.length; v++){
+        if(videos[v].src.length){
+            return videos[v].src;
+        }
+    }
+    return null;
+}
+module.exports = (videoUrl, callback) => {
+    var headless = require("./headless.json");
+    var parsedUrl = url.parse(videoUrl);
+    if(typeof headless[parsedUrl.host] === "undefined"){
+        firstParsing(parsedUrl, (err, headlessOnly, video) => {
+            if(err){
+                callback(err);
+            }
+            else{
+                callback(null, video);
+                headless[parsedUrl.host] = headlessOnly;
+                fs.writeFileSync(__dirname + "/headless.json", JSON.stringify(headless));
+            }
+        });
+    }
+    else if(headless[parsedUrl.host]){
+        headlessParsing(parsedUrl.href, callback);
+    }
+    else{
+        parsing(parsedUrl.href, callback);
+    }
+};
+function headlessParsing(videoUrl, callback){
+    Nightmare({ show: false})
+    .useragent(userAgent)
+    .goto(videoUrl)
+    .evaluate(getVideo)
+    .end()
+    .then(function(video){
+        callback(video ? null : "Sorry, we couldn't find any video.", video);
+    })
+    .catch(callback);
+}
+function parsing(videoUrl, callback){
+    request({"url": videoUrl, "User-Agent": userAgent}, (err, res, html) => {
+        if(err){
+            callback(err);
+        }
+        else{
+            parsonic.load(html, {}, getVideo, (results) => {
+                callback(results.error || null, results.error ? null : results);
+            });
+        }
+    })
+}
+function firstParsing(parsedUrl, callback){
+    var filename = __dirname + "/tmp/" + encodeURIComponent(parsedUrl.href) + ".html";
+    Nightmare({ show: false})
+    .useragent(userAgent)
+    .goto(parsedUrl.href)
+    .html(filename, "HTMLOnly")
+    .evaluate(getVideo)
+    .end()
+    .then(function(video){
+        fs.readFile(filename,
+        (err, html) => {
+            try{
+                if(err){
+                    throw err;
+                }
+                parsonic.load(html, {}, getVideo, (results) => {
+                    if(results){
+                        if(results.error){
+                            throw results.error;
+                        }
+                        else{
+                            callback(null, false, results);
+                        }
+                    }
+                    else if(video){
+                        callback(null, true, video);
+                    }
+                    else{
+                        throw "Sorry, we couldn't find any video.";
+                    }
+                });
+            }
+            catch(err){
+                callback(err);
+            }
+            fs.unlinkSync(filename);
+        });
+    })
+    .catch(callback);
+}
